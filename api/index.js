7cec48671f286c985b800330a8ccacc60e09b281
Matthieu Resurreccion <matthieu.resurreccion.531@my.csun.edu>
2025-06-04 19:18:34 -0700

    integrated backend api for register and login

---
+++ api/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const User = require('./models/User');
+
+dotenv.config();
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // exit the process if db fails to connect
+  }
+})();
+const jwtSecret = process.env.JWT_SECRET;
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+});
+
+app.get('/profile', (req, res) => {
+    const token = req.cookies?.token;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+            if (err) throw err;
+            res.json(userData);
+        });
+    } else {
+       res.status(401).json('no token'); 
+    }
+});
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const createdUser = await User.create({username, password});
+        jwt.sign({userId: createdUser._id, username}, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token, {sameSite: 'none', secure: true}).status(201).json({
+                id: createdUser._id,
+            });
+        });
+    } catch(err) {
+        if (err) throw err;
+        res.status(500).json('error');
+    }
+});
+
+app.listen(4040);
+
+// Database password: ag5TTUnOEs8jY2Bp
