74e5692971f9e8b84fb172175733472d6b99b2fb
Jose <jose.bastidasgonzalez.748@my.csun.edu>
2025-07-03 14:39:29 -0700

    Updated Login/Signup page.

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -5,6 +5,7 @@ import { UserContext } from "./UserContext";
 import uniqBy from "lodash/uniqBy";
 import axios  from "axios";
 import Contact from "./Contact";
+import './indexTwo.css'
 
 
 export default function Chat(){
--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -1,6 +1,9 @@
 import {useState, useContext} from "react";
 import axios from "axios"
 import {UserContext} from "./UserContext.jsx";
+import './index.css'
+import google from './assets/google.svg';
+import apple from './assets/apple.svg';
 
 export default function RegisterAndLoginForm() {
     const [username, setUsername] = useState('');
@@ -46,40 +49,99 @@ export default function RegisterAndLoginForm() {
 
     // Login/Register page HTML/CSS
     return (
-        <div className="bg-blue-50 h-screen flex items-center">
-            <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
+        <div className="login-container">
+            
+
+            <h2 className="form-title">Log in with</h2>
+
+            
+            <div className="social-login">
+
+
+                <button className="social-button">
+
+                    <img src= {google} alt="Google" className="social-icon" />
+                        Google
+
+                 </button>
+
+                <button className="social-button">
+
+                        <img src={apple} alt="Apple" className="social-icon" />
+                        Apple
+
+                </button>
+
+
+            </div>
+
+            <p className="separator"><span>or</span></p>
+
+            
+            <form  action="#" className="login-form" onSubmit={handleSubmit}>
+               
+               <div className="input-wrapper">
+
                 <input value={username} 
                        onChange={ev => setUsername(ev.target.value)} 
-                       type="text" 
-                       placeholder="username" 
-                       className="block w-full rounded-sm p-2 mb-2 border"/>
+                       type="email" 
+                       placeholder="Email address" 
+                       className="input-field"/>
+
+                <i className="material-symbols-rounded">mail</i>
+                       
+                </div>
+                
+
+                <div className="input-wrapper">
+
                 <input value={password} 
                        onChange={ev => setPassword(ev.target.value)} 
                        type="password" 
-                       placeholder="password" 
-                       className="block w-full rounded-sm p-2 mb-2 border"/>
-                <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+                       placeholder="Password" 
+                       className="input-field"/>
+
+                <i className="material-symbols-rounded">lock</i>
+
+               </div>
+               
+                <button className="login-button">
                     {isLoginOrRegister === 'register' ? 'Register' : 'Login'}
                 </button>
+                
+            
+                
                 <div className="text-center mt-2">
                     {isLoginOrRegister === 'register' && (
-                        <div>
+
+
+                        <p className="signup-text">
                         Already a member? 
-                        <button onClick={() => setIsLoginOrRegister('login')}>
+                        <a href="#" onClick={() => setIsLoginOrRegister('login')}>
                             Login here
-                        </button>
-                        </div>    
+                        </a>
+                        </p>    
+
+
                     )}
                     {isLoginOrRegister === 'login' && (
-                        <div>
+
+
+                        <p className="signup-text">
                             Don't have an account?
-                            <button onClick={() => setIsLoginOrRegister('register')}>
+                            <a href="#" onClick={() => setIsLoginOrRegister('register')}>
                                 Register
-                            </button>
-                        </div>
+                            </a>
+                        </p>
+
+
                     )}
                 </div>
+
+
             </form> 
+
+
         </div>
     );
 }
